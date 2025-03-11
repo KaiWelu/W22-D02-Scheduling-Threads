f298f12da36fbae969885d760ac91bd93f672992
KaiWelu <kaiweluda@gmail.com>
2025-03-11 15:22:14 +0100

    tasks 3+4

--- labs/scheduling/src/main/java/org/dci/Main.java
+++ labs/scheduling/src/main/java/org/dci/Main.java
@@ -26,11 +26,45 @@ public class Main {
         };
 
         ScheduledExecutorService service2 = Executors.newScheduledThreadPool(1);
-        service2.scheduleAtFixedRate(task2, 2, 3, TimeUnit.SECONDS);
+        service2.scheduleAtFixedRate(task2, 1, 3, TimeUnit.SECONDS);
 
-        if(!service2.awaitTermination(10, TimeUnit.SECONDS)) {
-            System.out.println("Shutting down after 10 Seconds");
+        if(!service2.awaitTermination(5, TimeUnit.SECONDS)) {
+            System.out.println("Shutting down after 5 Seconds");
             service2.shutdownNow();
         }
+
+        //Task3
+        Runnable test1 = () -> {
+            System.out.println("This is the test1 task");
+        };
+        Runnable test2 = () -> {
+            System.out.println("This is the test2 task");
+        };
+        Runnable test3 = () -> {
+            System.out.println("This is the test3 task");
+        };
+
+        ScheduledExecutorService service3 = Executors.newScheduledThreadPool(1);
+        service3.schedule(test1, 2, TimeUnit.SECONDS);
+        service3.schedule(test2, 1, TimeUnit.SECONDS);
+        service3.schedule(test3, 7, TimeUnit.SECONDS);
+
+        service3.shutdown();
+        if(!service3.awaitTermination(5, TimeUnit.SECONDS)) {
+            System.out.println("Forced shutdown after 5 seconds");
+            service3.shutdownNow();
+        }
+
+        //Task4
+
+        Callable<Double> task4 = () -> {
+            System.out.println("Calculating random sum...");
+            return (Math.random() + Math.random());
+        };
+
+        ExecutorService service4 = Executors.newFixedThreadPool(1);
+        Future<Double> sumFuture = service4.submit(task4);
+        System.out.println(sumFuture.get());
+        service4.shutdown();
     }
 }
